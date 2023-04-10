8278eae34f5cc70fea56bd6389dfacba2a6f986a
Creastv <piotr@roial.pl>
2023-04-10 10:42:15 +0200

    css framework setup

---
+++ src/App-old.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/header/Navbar";
+import Login from "./pages/Login";
+
+function App() {
+  return(
+    <div>
+      <Navbar />
+      <Login />
+    </div>
+  );
+}
+export default App;
